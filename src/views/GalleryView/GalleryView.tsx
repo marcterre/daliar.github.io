7b4d213f789c3b45465f90262a128ddbe49dfd21
marcterre <marc.terre@outlook.de>
2023-06-05 00:28:55 +0200

    add comments and date to dummy images

--- src/views/GalleryView/GalleryView.tsx
+++ src/views/GalleryView/GalleryView.tsx
@@ -1,11 +1,17 @@
-import data from "../../utils/db.json";
+import images from "../../utils/db.json";
 import "./Gallery.scss";
 
 export const GalleryView = () => {
   return (
     <div className="galleryWrapper" id="gallery">
-      {data.map((dat, index) => (
-        <img src={dat.url} alt="something" key={index} />
+      {images.map((image, index) => (
+        <div className="image-wrapper">
+          <img src={image.url} alt="something" key={index} />
+          <div className="overlay">
+            <article>{image.comment}</article>
+            <p>{image.date}</p>
+          </div>
+        </div>
       ))}
     </div>
   );
