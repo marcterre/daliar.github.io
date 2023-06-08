095be15e5dc635ce272cc346c5b29d5211a8527b
marcterre <marc.terre@outlook.de>
2023-06-09 00:44:33 +0200

    add hover effect to gallery

--- src/views/GalleryView/GalleryView.tsx
+++ src/views/GalleryView/GalleryView.tsx
@@ -1,16 +1,30 @@
 import images from "../../utils/db.json";
 import "./Gallery.scss";
+import useContent from "../../hooks/useContent";
 
 export const GalleryView = () => {
+  const [showContent, handleToggleContent] = useContent(images);
+
   return (
     <div className="galleryWrapper" id="gallery">
       {images.map((image, index) => (
-        <div className="image-wrapper">
-          <img src={image.url} alt="something" key={index} />
-          <div className="overlay">
-            <article>{image.comment}</article>
-            <p>{image.date}</p>
-          </div>
+        <div className="image-wrapper" key={index}>
+          {showContent ? (
+            <></>
+          ) : (
+            <div className="overlay">
+              <article>{image.comment}</article>
+              <p>{image.date}</p>
+            </div>
+          )}
+          <figure>
+            <img
+              src={image.url}
+              alt="something"
+              key={index}
+              onMouseEnter={() => handleToggleContent}
+            />
+          </figure>
         </div>
       ))}
     </div>
