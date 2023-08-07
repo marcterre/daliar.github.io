baf87e172de031c266dfeb212345df35c03dd752
m.terre <m.terre@it-excelsus.de>
2023-08-07 11:15:27 +0200

    fix image display

--- src/views/GalleryView/GalleryView.tsx
+++ src/views/GalleryView/GalleryView.tsx
@@ -1,40 +1,36 @@
 import images from "../../utils/db.json";
 import "./GalleryView.styles.scss";
-import { Button } from "../../components/Inputs";
-import { useEffect, useState } from "react";
-import { ImageModal } from "./partials/ImageModal";
+import { useEffect } from "react";
+import useImageStore from "../../stores/useImageStore";
 
 export const GalleryView = () => {
-  const { setImages } = useImageStore();
-  const [openModal, setOpenModal] = useState(false);
+	const { setImages } = useImageStore();
 
-  useEffect(() => {
-    setImages(images);
-  }, [setImages]);
+	useEffect(() => {
+		setImages(images);
+	}, [setImages]);
 
-  return (
-    <div className="gallery-wrapper" id="gallery">
-      {images.map((image, index) => (
-        <div className="image-wrapper" key={index}>
-          <img
-            className="image-content"
-            src={image.url}
-            alt="something"
-            key={index}
-          />
-          <Button
-            variant="image-expand"
-            handleClick={() => setOpenModal(true)}
-            label="View Details"
-          />
-          {openModal && <ImageModal />}
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div className="gallery-wrapper" id="gallery">
+			{images.map((image, index) => {
+				const { url, comment, date } = image;
+				return (
+					<div
+						className={`image-wrapper ${comment ? "image-wrapper-hover" : ""}`}
+						key={index}
+					>
+						<img className="image-content" src={url} alt="something" key={index} />
+						{comment && (
+							<div className="image-wrapper-middle">
+								<p>{comment}</p>
+								<p>{date}</p>
+							</div>
+						)}
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export default GalleryView;
-function useImageStore(): { images: any; setImages: any } {
-  throw new Error("Function not implemented.");
-}
