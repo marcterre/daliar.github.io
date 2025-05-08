45a6774f95d192367919defd0c386836731f3da9
marcterre <marc.terre@outlook.de>
2025-05-08 12:29:39 +0000

    chore: init supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,19 @@
-"use client";
-import CreateProjectFormComponent from "@/components/layout/CreateProjectFormComponent";
-import HeaderComponent from "@/components/layout/HeaderComponent";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
 
-export type ProjectType = {
-  _id?: string;
-  title: string;
-  description: string;
-  image: {
-    url: string;
-    id: string;
-  };
-};
+export default async function Home() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
 
-export type UpdateProjectType = {
-  foundProject: ProjectType;
-};
-
-export default function Home() {
-  // const data = await getProjects();
-  const { user } = useUser();
-
-  const getProjects = async () => {
-    try {
-      const res = await fetch("http://localhost:3000/api/projects", {
-        cache: "no-store",
-      });
-
-      if (!res.ok) {
-        throw new Error("Failed to fetch topics");
-      }
-
-      return res.json();
-    } catch (error) {
-      console.log("Error loading topics: ", error);
-    }
-  };
+  const { data: projects } = await supabase.from('projects').select('*')
 
   return (
     <main>
-      <div className="h-screen">
-        <HeaderComponent />
-        <div className="grid justify-center content-center gap-4">
-          <h1 className="text-7xl">Lia Dingeldein</h1>
-          <h2 className="text-4xl">Character designer</h2>
-          {user && <CreateProjectFormComponent />}
-        </div>
-      </div>
+      <ul>
+        {projects?.map((project) => (
+          <li key={project.id}>{project.title}</li>
+        ))}
+      </ul>
     </main>
-  );
+  )
 }
